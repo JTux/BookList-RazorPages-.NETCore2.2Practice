ab0d89887a0bce88026e655a748b736978ee1fc2
Joshua Tucker <thejltucker@gmail.com>
2019-10-22 16:06:41 -0400

    Edit Page with functionality added

---
+++ BookListRazor/Pages/BookList/Edit.cshtml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BookListRazor.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace BookListRazor.Pages.BookList
+{
+    public class EditModel : PageModel
+    {
+        private readonly ApplicationDbContext _context;
+        public EditModel(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        [BindProperty]
+        public Book Book { get; set; }
+
+        public async Task OnGet(int id)
+        {
+            Book = await _context.Books.FindAsync(id);
+        }
+
+        public async Task<IActionResult> OnPost()
+        {
+            if (!ModelState.IsValid)
+                return Page();
+
+            var bookFromDb = await _context.Books.FindAsync(Book.Id);
+
+            bookFromDb.Title = Book.Title;
+            bookFromDb.Author = Book.Author;
+            bookFromDb.ISBN = Book.ISBN;
+
+            await _context.SaveChangesAsync();
+
+            return RedirectToPage("Index");
+        }
+    }
+}
